5d2418871aa6a207a0e29050dc58dce42d859c06
vinhcaugo5d-blip <vinhcaugo5d@gmail.com>
2026-07-25 21:04:10 +0700

    Create Personality.ts

---
+++ server/src/ai/Personality.ts
@@ -0,0 +1,19 @@
+export enum Personality {
+
+    AGGRESSIVE="AGGRESSIVE",
+
+    CALM="CALM",
+
+    SMART="SMART",
+
+    SHY="SHY",
+
+    TALKATIVE="TALKATIVE",
+
+    LIAR="LIAR",
+
+    CHAOTIC="CHAOTIC",
+
+    LEADER="LEADER"
+
+}
