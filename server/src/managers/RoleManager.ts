207f137fba60fa2494364fecfef00013f13f4673
vinhcaugo5d-blip <vinhcaugo5d@gmail.com>
2026-07-25 21:02:28 +0700

    Create RoleManager.ts

---
+++ server/src/managers/RoleManager.ts
@@ -0,0 +1,37 @@
+import { Role } from "../core/Role";
+
+export class RoleManager {
+
+    public static generateRoles() {
+
+        return [
+
+            Role.WEREWOLF,
+
+            Role.WEREWOLF,
+
+            Role.SEER,
+
+            Role.WITCH,
+
+            Role.HUNTER,
+
+            Role.GUARD,
+
+            Role.VILLAGER,
+
+            Role.VILLAGER,
+
+            Role.VILLAGER,
+
+            Role.VILLAGER,
+
+            Role.VILLAGER,
+
+            Role.VILLAGER
+
+        ];
+
+    }
+
+}
