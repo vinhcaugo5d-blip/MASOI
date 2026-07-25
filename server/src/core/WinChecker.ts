07b384339f402642905981ebc5327df34d85f949
vinhcaugo5d-blip <vinhcaugo5d@gmail.com>
2026-07-25 21:01:05 +0700

    Create WinChecker.ts

---
+++ server/src/core/WinChecker.ts
@@ -0,0 +1,24 @@
+/**
+ * ============================================================
+ * Win Checker
+ * ============================================================
+ */
+
+export class WinChecker {
+
+    public static check() {
+
+        /**
+         * Giai đoạn sau sẽ viết:
+         *
+         * - Dân thắng
+         * - Sói thắng
+         * - Piper thắng
+         * - Cupid thắng
+         */
+
+        return null;
+
+    }
+
+}
