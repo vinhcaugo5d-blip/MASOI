3d0da6907b48ed29df2f079fc500fe6ceb4bc72e
vinhcaugo5d-blip <vinhcaugo5d@gmail.com>
2026-07-25 21:00:41 +0700

    Create Phase.ts

---
+++ server/src/core/Phase.ts
@@ -0,0 +1,25 @@
+/**
+ * ============================================================
+ * Phase
+ * ============================================================
+ */
+
+export enum Phase {
+
+    LOBBY = "LOBBY",
+
+    PREPARE = "PREPARE",
+
+    NIGHT = "NIGHT",
+
+    MORNING = "MORNING",
+
+    DISCUSSION = "DISCUSSION",
+
+    VOTING = "VOTING",
+
+    EXECUTION = "EXECUTION",
+
+    GAME_OVER = "GAME_OVER"
+
+}
